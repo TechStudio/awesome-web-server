df78c2d76be7a1182507f4ae1c83fe0c4c2abb4e
Ryan Burnette <ryan.burnette@gmail.com>
2013-12-05 15:14:17 +0000

    First v of send_email, started config.yml

--- lib/send_email.rb
+++ lib/send_email.rb
@@ -1,13 +1,18 @@
 require 'net/smtp'
 
-message = <<MESSAGE_END
-From: Awesome Admin <ryan.burnette@gmail.com>
-To: Ryan Burnette <ryan.burnette@gmail.com>
-Subject: SMTP email test
+def ts_send_email(from_name,from,reply_to_name,reply_to,to_name,to,subject,message_body)
+  message = <<MESSAGE_END
+From: #{from_name} <#{from}>
+Reply-To: #{reply_to_name} {#{reply_to}>
+To: #{to_name} <#{to}>
+Subject: #{subject}
 
-This is a test email message.
+#{message_body}
 MESSAGE_END
-
-Net::SMTP.start('localhost') do |smtp|
-  smtp.send_message message, 'ryan.burnette@gmail.com'
+  
+  Net::SMTP.start('localhost') do |smtp|
+    smtp.send_message message, from, to
+  end
 end
+
+ts_send_email('Awesome Admin','no-reply@awesome.techstud.io','Ryan Burnette','ryan.burnette@gmail.com','Ryan Burnete','ryan.burnette@gmail.com','Test','This is a test.')
