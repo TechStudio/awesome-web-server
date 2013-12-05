2fd6adc695727d6e70cb0d6351cc924c7dcf73e9
Ryan Burnette <ryan.burnette@gmail.com>
2013-12-05 18:21:15 +0000

    Working

---
+++ lib/tests/check_free_memory.rb
@@ -0,0 +1,36 @@
+require 'filesize'
+require 'ap'
+
+class Numeric
+  def percent_of(n)
+    self.to_f / n.to_f * 100.0
+  end
+end
+
+def check_free_memory
+  results = {}
+
+  free = `free -b`
+
+  memory_warn = $config['resources']['memory_warn']
+  memory_danger = $config['resources']['memory_danger']
+
+  memory_total = free.split(' ')[7].to_i
+  memory_free = free.split(' ')[9].to_i
+  memory_total_pretty = Filesize.from(memory_total.to_s + ' B').pretty
+  memory_free_pretty = Filesize.from(memory_free.to_s + ' B').pretty
+
+  memory_percent_free = memory_free.percent_of(memory_total).round
+
+  if memory_percent_free > memory_warn
+    results[:result] = 2
+  elsif memory_percent_free <= memory_warn && memory_percent_free > memory_danger
+    results[:result] = 1
+  else
+    results[:result] = 0
+  end
+
+  results[:message] = "System memory: #{memory_free_pretty} of a total #{memory_total_pretty} (#{memory_percent_free}%) free."
+
+  return results
+end
---
+++ test.rb
@@ -0,0 +1,37 @@
+require 'colorize'
+require 'ap'
+require './lib/send_email.rb'
+require 'yaml'
+
+$config = YAML.load_file('./config.yml')
+
+tests = []
+
+Dir["./lib/tests/*.rb"].each do |file|
+  require file
+  test_name = tests.push(file.sub('./lib/tests/','').sub('.rb',''))
+end
+
+tests.each do |test|
+  print test.gsub('_',' ').capitalize + ': '
+
+  test_result = send(test)
+
+  if test_result[:result] == 2
+    print 'Pass'.green
+  elsif test_result[:result] == 1
+    print 'Warn'.yellow
+  elsif test_result[:result] == 0
+    print 'Fail'.red
+  else
+    print test_result[:result]
+  end
+
+  if test_result[:result] != 2 
+    print "\n\n"
+    print test_result[:message]
+    print "\n\n"
+  end
+
+  print "\n"
+end
