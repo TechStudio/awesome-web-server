09d9e879acba0df6792c612fd8610d925dfc9b53
Ryan Burnette <ryan.burnette@gmail.com>
2013-12-05 21:10:21 +0000

    Organizing

---
+++ lib/configure/postfix.sh
@@ -0,0 +1,12 @@
+# remove interface line from main.cf
+sed -i.old "/inet_interfaces/d" /etc/postfix/main.cf
+
+sleep 1
+
+# add localhost interface only
+echo "inet_interfaces = 127.0.0.1" >> /etc/postfix/main.cf
+
+sleep 1
+
+# restart
+service postfix restart
