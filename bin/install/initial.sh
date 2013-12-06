5496cbb4f1c5e5a59ed686305c127b0a4f7aae41
Ryan Burnette <ryan.burnette@gmail.com>
2013-12-06 01:07:58 +0000

    Working

---
+++ bin/install/initial.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get install --yes \
+  vim \
+  screen
+
+sudo update-alternatives --config editor
+
+sudo apt-get install --yes \
+  rsync \
+  wget \
+  curl \
+  zsh \
+  git \
+  htop \
+  build-essential
