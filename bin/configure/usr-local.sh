26d432cfeaf39fd786e89a8647d29dcb25f34282
Ryan Burnette <ryan.burnette@gmail.com>
2013-12-23 13:33:22 -0500

    Update usr-local.sh

--- bin/configure/usr-local.sh
+++ bin/configure/usr-local.sh
@@ -1,5 +1,5 @@
 # usr local should be owned by the admin group
-chgrp -R admin /usr/local
+chgrp -R adm /usr/local
 
 # usr local should have 7 permissions for the admin group
 chmod -R g=u /usr/local
