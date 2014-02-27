d213bbba85fa1675e3fa3b52f4fc9c5cfe925980
Ryan Burnette <ryan.burnette@gmail.com>
2014-02-27 10:21:05 -0500

    Removed unneeded stuff, misc updates

--- chef-install.sh
+++ chef-install.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install curl
+sudo apt-get -y -q install curl
 curl -L http://www.opscode.com/chef/install.sh | sudo bash
--- cookbooks/initial/recipes/update_and_install_essential_apt_packages.rb
+++
@@ -1,8 +0,0 @@
-execute "update apt" do
-  sudo apt-get update
-end
-
-execute "install the essentials" do
-  sudo apt-get install --yes vim screen rsync wget curl zsh git htop build-essential
-  #sudo update-alternatives --config editor
-end
