ed3af2cc43bbffbc32c6cac3f1f43d59db6b4425
Ryan Burnette <ryan.burnette@gmail.com>
2014-02-27 10:32:14 -0500

    Simplified initial recipe

--- cookbooks/initial/recipes/default.rb
+++ cookbooks/initial/recipes/default.rb
@@ -5,9 +5,19 @@ execute "updateapt" do
   #not_if {File.exists?("#{Chef::Config[:file_cache_path]}/updateapt")}
 end
 
-#file "#{Chef::Config[:file_cache_path]}/updateapt" do
-#  owner "root"
-#  group "root"
-#  mode "0600"
-#  action :create
-#end
+file "#{Chef::Config[:file_cache_path]}/updateapt" do
+  owner "root"
+  group "root"
+  mode "0600"
+  action :touch
+end
+
+list = ['vim', 'screen', 'rsync', 'wget', 'curl', 'zsh', 'git', 'htop', 'build-essential']
+
+list.each do |p|
+  package "#{p}" do
+    action :install
+  end
+end
+
+# set default editor to vim
