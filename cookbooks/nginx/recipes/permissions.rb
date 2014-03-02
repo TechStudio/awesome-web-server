280e6870f8522427767131fa0cca68fda9888867
Ryan Burnette <ryan.burnette@gmail.com>
2014-03-02 12:56:39 -0500

    Updated nginx recipe

---
+++ cookbooks/nginx/recipes/permissions.rb
@@ -0,0 +1,8 @@
+execute "set_www_permissions" do
+  command "chown -R www-data:adm /var/www; chmod -R 774 /var/www"
+  user "root"
+end
+
+# Use template to create script
+# Create cron job to run this every minute
+# Consider totally different approach
