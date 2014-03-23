2e20820f83f388b98c7f976cca77382c86b51546
Ryan Burnette <ryan.burnette@gmail.com>
2014-03-22 20:51:22 -0400

    Replacing all this with Monit later

--- _old/lib/tests/_apache2.rb
+++
@@ -1,5 +0,0 @@
-# apache2 tests
-
-  # make sure expose_php directive is set to Off
-
-  # make sure version numbers are hidden, ServerSignature should be Off ServerTokens should be Prod
--- _old/lib/tests/_nginx.rb
+++
@@ -1,6 +0,0 @@
-# nginx tests
-
-  # test /etc/nginx/nginx.conf (and includes)
-
-  # test /etc/nginx/sites-enabled/*
-    # test to make sure .ht* or .* is denied
