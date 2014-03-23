a809e6aa0a0a3b7c737bec6950318915e12cb4a0
Ryan Burnette <ryan.burnette@gmail.com>
2014-03-22 20:34:25 -0400

    Nginx vhost templates

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -17,7 +17,47 @@ end
 
 template "/etc/nginx/nginx.conf" do
   source "nginx.conf.erb"
-  mode 0770
+  mode 0644
+  owner "root"
+  group "adm"
+  not_if "test -f #{Chef::Config[:file_cache_path]}/nginx_lock"
+end
+
+template "/etc/nginx/sites-available/apache2.conf" do
+  source "apache2.conf.erb"
+  mode 0644
+  owner "root"
+  group "adm"
+  not_if "test -f #{Chef::Config[:file_cache_path]}/nginx_lock"
+end
+
+template "/etc/nginx/sites-available/http_auth.conf" do
+  source "http_auth.conf.erb"
+  mode 0644
+  owner "root"
+  group "adm"
+  not_if "test -f #{Chef::Config[:file_cache_path]}/nginx_lock"
+end
+
+template "/etc/nginx/sites-available/php.conf" do
+  source "php.conf.erb"
+  mode 0644
+  owner "root"
+  group "adm"
+  not_if "test -f #{Chef::Config[:file_cache_path]}/nginx_lock"
+end
+
+template "/etc/nginx/sites-available/redirect.conf" do
+  source "redirect.conf.erb"
+  mode 0644
+  owner "root"
+  group "adm"
+  not_if "test -f #{Chef::Config[:file_cache_path]}/nginx_lock"
+end
+
+template "/etc/nginx/sites-available/static.conf" do
+  source "static.conf.erb"
+  mode 0644
   owner "root"
   group "adm"
   not_if "test -f #{Chef::Config[:file_cache_path]}/nginx_lock"
