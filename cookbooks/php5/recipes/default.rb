14fae40d7f9d6fec75cfbde040f025a9526804c5
Ryan Burnette <ryan.burnette@gmail.com>
2014-09-29 10:38:09 -0400

    fixed issue w/ php5-fpm blanking on nginx

--- cookbooks/php5/recipes/default.rb
+++ cookbooks/php5/recipes/default.rb
@@ -18,6 +18,14 @@ template "/etc/php5/fpm/php.ini" do
   not_if "test -f #{Chef::Config[:file_cache_path]}/php5_lock"
 end
 
+template "/etc/php5/fpm/pool.d/" do
+  source "www.conf.erb"
+  mode 0644
+  owner "root"
+  group "adm"
+  not_if "test -f #{Chef::Config[:file_cache_path]}/php5_lock"
+end
+
 file "#{Chef::Config[:file_cache_path]}/php5_lock" do
   owner "root"
   group "root"
