6dffe2342ccdaea05eb663ec0b95557f4c2bf89a
Ryan Burnette <ryan.burnette@gmail.com>
2014-12-05 14:58:34 -0500

    Update get cookbooks script

---
+++ get-cookbooks.sh
@@ -0,0 +1,9 @@
+wget https://github.com/chef-ubuntu/nginx/tarball/master
+tar zxf master
+mv chef-ubuntu-nginx* cookbooks/nginx
+rm master
+
+wget https://github.com/chef-ubuntu/postgresql/tarball/master
+tar zxf master
+mv chef-ubuntu-postgr* cookbooks/postgres
+rm master
--- install-cookbooks.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/chef-ubuntu/nginx/tarball/master
-tar -zxf master
-mv chef-ubuntu-nginx* cookbooks/nginx
-rm master
