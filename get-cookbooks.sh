3cc6cc29a4f24616404f8126afddca55e8ca0031
Ryan Burnette <ryan.burnette@gmail.com>
2014-12-07 08:59:10 -0500

    Update get cookbooks sh

--- get-cookbooks.sh
+++ get-cookbooks.sh
@@ -7,3 +7,8 @@ wget https://github.com/chef-ubuntu/postgresql/tarball/master
 tar zxf master
 mv chef-ubuntu-postgr* cookbooks/postgres
 rm master
+
+wget https://github.com/chef-ubuntu/ruby/tarball/master
+tar zxf master
+mv chef-ubuntu-ruby* cookbooks/ruby
+rm master
