250e0d7ed9ba0ba3dc3925dd5151c23d4b0a7f1b
Ryan Burnette <ryan.burnette@gmail.com>
2015-01-03 12:07:25 -0500

    Improving cookbook installer

--- install-chef-solo.sh
+++ install-chef-solo.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget http://www.opscode.com/chef/install.sh
 chmod +x install.sh
 sudo ./install.sh
--- install-cookbooks.sh
+++ install-cookbooks.sh
@@ -1,29 +1,5 @@
-wget https://github.com/chef-ubuntu/nginx/tarball/master
-tar zxf master
-mv chef-ubuntu-nginx* cookbooks/nginx
-rm master
-
-wget https://github.com/chef-ubuntu/postgresql/tarball/master
-tar zxf master
-mv chef-ubuntu-postgr* cookbooks/postgres
-rm master
-
-wget https://github.com/chef-ubuntu/ruby/tarball/master
-tar zxf master
-mv chef-ubuntu-ruby* cookbooks/ruby
-rm master
-
-wget https://github.com/chef-ubuntu/node/tarball/master
-tar zxf master
-mv chef-ubuntu-node* cookbooks/node
-rm master
-
-wget https://github.com/chef-ubuntu/imagemagick/tarball/master
-tar zxf master
-mv chef-ubuntu-imagemagick* cookbooks/imagemagick
-rm master
-
-wget https://github.com/chef-ubuntu/sqlite3/tarball/master
-tar zxf master
-mv chef-ubuntu-sqlite3* cookbooks/sqlite3
-rm master
+#!/usr/bin/env bash
+for cookbook in essentials fish nginx postgresql ruby node imagemagick sqlite3
+do
+  ./get-cookbook.sh $cookbook
+done
