26465ebbeeb251e2966451bbbda669525fbdcf86
Ryan Burnette <ryan.burnette@gmail.com>
2015-01-16 09:44:40 -0500

    update ruby installers

--- install-cookbooks.sh
+++ install-cookbooks.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for chef_ubuntu_cookbook in essentials fish nginx postgresql ruby ruby-install node imagemagick sqlite3 redis-server
+for chef_ubuntu_cookbook in essentials fish nginx postgresql ruby-brightbox ruby-install node imagemagick sqlite3 redis-server
 do
   ./get-chef-ubuntu-cookbook.sh $chef_ubuntu_cookbook
 done
