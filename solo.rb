4d5648a6059d80e7a70665ef2273f69a6546d8a2
Ryan Burnette <ryan.burnette@gmail.com>
2015-01-17 14:12:20 -0500

    Not using it, but might as well set it

--- solo.rb
+++ solo.rb
@@ -2,6 +2,6 @@ root = File.absolute_path(File.dirname(__FILE__))
 file_cache_path root
 cookbook_path root + '/cookbooks'
 data_bag_path root + '/data_bags'
-#environment_path root + '/environments'
+environment_path root + '/environments'
 file_cache_path = '/var/chef/cache'
 Chef::Config[:file_cache_path]= '/var/chef/cache'
